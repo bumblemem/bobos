a5f594ced74e08be36aab026afd2d453d04b9978
bumblemem <100539077+bumblemem@users.noreply.github.com>
2022-02-27 23:50:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+  const res = http.get(__ENV.URL);
+  //
+  check(res, { 'status was 200': (r) => r.status == 200 });
+}
